204985d44c5ae05804fb7487f6ee5d98f484eebb
ATANU JANA <65564188+atanu20@users.noreply.github.com>
2021-12-30 19:18:06 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Online Book Store website design</title>
-        <link rel="icon" type="image/png" href="book-1.png"></link>
+        <link rel="icon" type="image/png" href="images/book-1.png"></link>
       </Head>
       <Component {...pageProps} />
     </>
